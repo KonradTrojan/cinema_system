969e1aefb22461ee85a1477d2b3f2a70215f66d7
Konrad Trojan <konrad.trojan99@gmail.com>
2020-04-15 17:08:40 +0200

    Artur ogarnij to

--- src/com/access/admin/Movies.java
+++ src/com/access/admin/Movies.java
@@ -1,4 +1,28 @@
 package com.access.admin;
 
-public class Movies {
+import java.sql.*;
+
+public abstract class Movies {
+    Statement stmt;
+    Connection con;
+
+    private void openDataBase(){
+        String url = "jdbc:mysql://remotemysql.com:3306/w5NtVBqSWa";
+        String user = "w5NtVBqSWa";
+        String password = "F6mKAri9L0";
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            this.con = DriverManager.getConnection(
+                    url, user, password);
+
+            this.stmt = con.createStatement();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+    private void closeDataBase() throws SQLException {
+        con.close();
+    }
 }
