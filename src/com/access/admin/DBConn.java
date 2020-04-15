45b837ec6219d22d6797ed5813c2d49dd77e5f80
TrololoManPL <trololomanpl@mailplus.pl>
2020-04-15 17:41:05 +0200

    obsluga bazy DBConn

--- src/com/access/admin/DBConn.java
+++ src/com/access/admin/DBConn.java
@@ -1,12 +1,22 @@
 package com.access.admin;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class DBConn {
+    public static ResultSet execute (String query) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://remotemysql.com:3306/w5NtVBqSWa", "w5NtVBqSWa", "F6mKAri9L0");
 
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            //con.close();
 
+            return rs;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
-
--- src/main.java
+++ src/main.java
@@ -1,19 +1,32 @@
-import java.sql.*;
+import com.access.admin.DBConn;
+
+import java.sql.ResultSet;
 
 class Main {
     public static void main(String args[]) {
+        ResultSet rs = DBConn.execute("select * from test1");
         try {
-            Class.forName("com.mysql.jdbc.Driver").newInstance();
-            Connection con = DriverManager.getConnection(
-                    "jdbc:mysql://remotemysql.com:3306/w5NtVBqSWa", "w5NtVBqSWa", "F6mKAri9L0");
-
-            Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("select * from test1");
             while (rs.next())
                 System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3) + " " + rs.getString(4));
-            con.close();
-        } catch (Exception e) {
-            System.out.println(e);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        ResultSet rs2 = DBConn.execute("select * from test1");
+        try {
+            while (rs2.next())
+                System.out.println(rs2.getInt(1) + "  " + rs2.getString(2) + "  " + rs2.getString(3) + " " + rs2.getString(4));
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        ResultSet rs3 = DBConn.execute("select * from test1");
+        try {
+            while (rs3.next())
+                System.out.println(rs3.getInt(1) + "  " + rs3.getString(2) + "  " + rs3.getString(3) + " " + rs3.getString(4));
+        }
+        catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
