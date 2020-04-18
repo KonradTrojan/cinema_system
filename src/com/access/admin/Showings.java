8826fc2261c47eb7892b1747c9d4797432ceac87
TrololoManPL <trololomanpl@mailplus.pl>
2020-04-19 01:33:34 +0200

    dosc na dzis, zaczalem robic ShowingChooser

--- src/com/access/admin/Showings.java
+++ src/com/access/admin/Showings.java
@@ -1,9 +1,12 @@
 package com.access.admin;
 
+import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 public abstract class Showings {
-    int getRoomId(int id) {
+    public static int getRoomId(int id) {
         try {
             ResultSet rs = DBConn.execute("SELECT idRoom FROM filmScreenings WHERE idScreenings="+id);
             if (rs.next()) {
@@ -14,7 +17,7 @@ public abstract class Showings {
         }
         return 0;
     }
-    int getMovieId(int id) {
+    public static int getMovieId(int id) {
         try {
             ResultSet rs = DBConn.execute("SELECT idmovie FROM filmScreenings WHERE idScreenings="+id);
             if (rs.next()) {
@@ -24,5 +27,24 @@ public abstract class Showings {
             e.printStackTrace();
         }
         return 0;
+    }
+    public static ArrayList <Integer> getAllShowings(int id) {
+        ArrayList<Integer> x = new ArrayList<Integer>();
+        try {
+            Connection con = DBConn.getConnection();
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT idScreenings FROM filmScreenings");
+            while (rs.next()) {
+                x.add(rs.getInt("idScreenings"));
+            }
+            stmt.close();
+            return x;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return x;
+    }
+    public static int getNumberOfShowings(int id) {
+        return getAllShowings(id).size();
     }
 }
--- src/com/access/user/ShowingChooser.java
+++ src/com/access/user/ShowingChooser.java
@@ -1,4 +1,48 @@
 package com.access.user;
 
+import com.access.admin.Movies;
+import com.access.admin.Showings;
+import de.jaret.util.date.IntervalImpl;
+import de.jaret.util.date.JaretDate;
+import de.jaret.util.ui.timebars.model.DefaultRowHeader;
+import de.jaret.util.ui.timebars.model.DefaultTimeBarModel;
+import de.jaret.util.ui.timebars.model.DefaultTimeBarRowModel;
+import de.jaret.util.ui.timebars.model.TimeBarModel;
+import de.jaret.util.ui.timebars.swing.TimeBarViewer;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
 public class ShowingChooser {
+    public ShowingChooser(int movieId) {
+        this.movieId = movieId;
+        draw();
+    }
+
+    int movieId;
+
+    private void draw() {
+        JFrame f = new JFrame("Wybór seansu");
+        f.getContentPane().setLayout(new BorderLayout());
+        f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        DefaultTimeBarModel model = new DefaultTimeBarModel();
+        TimeBarViewer tbv = new TimeBarViewer(model);
+        DefaultRowHeader header = new DefaultRowHeader(Movies.getTitle(movieId));
+        DefaultTimeBarRowModel tbr = new DefaultTimeBarRowModel(header);
+        ArrayList <Integer> showings = Showings.getAllShowings(movieId);
+        for (int i = 0; i < showings.size(); i++) {
+            IntervalImpl interval = new IntervalImpl();
+            JaretDate start = new JaretDate();
+            JaretDate end = new JaretDate();
+            interval.setBegin(start.copy());
+            interval.setEnd(end.copy());
+            tbr.addInterval(interval);
+        }
+        model.addRow(tbr);
+        f.getContentPane().add(tbv, BorderLayout.CENTER);
+        f.pack();
+        f.setVisible(true);
+    }
 }
