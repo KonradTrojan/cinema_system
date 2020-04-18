d9f3f2237ac523e8c03c84ba362e99a9dc5442a7
TrololoManPL <trololomanpl@mailplus.pl>
2020-04-19 00:32:07 +0200

    dopracowany MovieChooser

--- src/com/access/user/MovieChooser.java
+++ src/com/access/user/MovieChooser.java
@@ -1,15 +1,22 @@
 package com.access.user;
 
+//the main window of user interface.
+
 import com.access.admin.Movies;
 import org.jdesktop.swingx.JXLabel;
-import org.jdesktop.swingx.JXPanel;
-import org.jdesktop.swingx.JXTextArea;
-
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
-import java.awt.image.BufferedImage;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
+
+class MovieButton extends JButton {
+    int movieId;
+
+    public MovieButton(String text) {
+        super(text);
+    }
+}
 
 public class MovieChooser {
     private JFrame mainFrame = new JFrame("Wybór filmu");
@@ -18,13 +25,13 @@ public class MovieChooser {
 
     private void draw() {
         int numberofmovies = Movies.getNumberOfMovies();
-        System.out.println(numberofmovies);
         ArrayList<Integer> movielist = Movies.getAllMovies();
         f.setLayout(new GridBagLayout());
         for (int i = 0; i < numberofmovies; i++) {
             drawSingleMovie(movielist.get(i), i);
         }
         JScrollPane scrollBar = new JScrollPane(f, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollBar.getVerticalScrollBar().setUnitIncrement(16);
         f.setAlignmentX(JPanel.LEFT_ALIGNMENT);
         mainFrame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         ImageIcon img = new ImageIcon("favicon.jpg");
@@ -33,31 +40,39 @@ public class MovieChooser {
         mainFrame.add(scrollBar);
         mainFrame.pack();
         mainFrame.setResizable(false);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int windowWidth = mainFrame.getWidth();
+        mainFrame.setBounds(screenSize.width / 2 - (windowWidth / 2), (screenSize.height / 2) - 700 / 2, windowWidth + 20, 700);
         mainFrame.setVisible(true);
     }
 
     private void drawSingleMovie(int movieid, int index) {
-        int xshift = index % 2 + 1;
+        int xshift; //0-left, 2-center, 4-right
+        if (index % 3 == 0)
+            xshift = 0;
+        else if (index % 3 == 1)
+            xshift = 2;
+        else
+            xshift = 4;
         //poster
         JLabel posterLabel = new JLabel(new ImageIcon(Movies.getPoster(movieid)));
         posterLabel.setBorder(BorderFactory.createTitledBorder("Plakat"));
         GridBagConstraints pC = new GridBagConstraints();
         pC.gridx = xshift;
-        pC.gridy = index/2 * 3;
+        pC.gridy = (index / 3) * 4;
         pC.ipady = 0;
         pC.ipadx = 0;
         pC.gridwidth = 1;
-        pC.gridheight = 3;
+        pC.gridheight = 4;
         pC.fill = GridBagConstraints.BOTH;
         pC.anchor = GridBagConstraints.NORTH;
         f.add(posterLabel, pC);
         //title
         JXLabel titleLabel = new JXLabel(Movies.getTitle(movieid));
         titleLabel.setBorder(BorderFactory.createTitledBorder("Tytuł"));
-        //titleLabel.setPreferredSize(new Dimension(200, titleLabel.getPreferredSize().height));
         GridBagConstraints tC = new GridBagConstraints();
         tC.gridx = 1 + xshift;
-        tC.gridy = index/2 * 3;
+        tC.gridy = (index / 3) * 4;
         tC.ipady = 0;
         tC.ipadx = 0;
         tC.gridwidth = 1;
@@ -67,10 +82,9 @@ public class MovieChooser {
         //director
         JXLabel directorLabel = new JXLabel(Movies.getDirector(movieid));
         directorLabel.setBorder(BorderFactory.createTitledBorder("Reżyseria"));
-        //directorLabel.setPreferredSize(new Dimension(200, directorLabel.getPreferredSize().height));
         GridBagConstraints dC = new GridBagConstraints();
-        dC.gridx = 1+xshift;
-        dC.gridy = 1 + index/2 * 3;
+        dC.gridx = 1 + xshift;
+        dC.gridy = 1 + (index / 3) * 4;
         dC.ipady = 0;
         dC.ipadx = 0;
         dC.gridwidth = 1;
@@ -78,16 +92,15 @@ public class MovieChooser {
         dC.fill = GridBagConstraints.BOTH;
         f.add(directorLabel, dC);
         //description
-        String x = "It's there, but it's preferred height is 0. That means it's too small to be visible. Change line 14 into these:";
-        JXLabel descriptionLabel = new JXLabel(Movies.getDescription(movieid) + x);
+        JXLabel descriptionLabel = new JXLabel(Movies.getDescription(movieid));
         descriptionLabel.setLineWrap(true);
         descriptionLabel.setMaxLineSpan(200);
         descriptionLabel.setBorder(BorderFactory.createTitledBorder("Opis"));
         descriptionLabel.setVerticalAlignment(SwingConstants.TOP);
-        //descriptionLabel.setPreferredSize(new Dimension(200, 270));
+        descriptionLabel.setPreferredSize(new Dimension(200, 270));
         GridBagConstraints deC = new GridBagConstraints();
-        deC.gridx = 1+xshift;
-        deC.gridy = 2 + index/2 * 3;
+        deC.gridx = 1 + xshift;
+        deC.gridy = 2 + (index / 3) * 4;
         deC.ipady = 0;
         deC.ipadx = 0;
         deC.gridwidth = 1;
@@ -97,19 +110,40 @@ public class MovieChooser {
         deC.anchor = GridBagConstraints.NORTH;
         deC.fill = GridBagConstraints.BOTH;
         f.add(descriptionLabel, deC);
-        /*if (index == Movies.getNumberOfMovies() - 1) {
+        //button
+        MovieButton chooseButton = new MovieButton("Wybierz");
+        chooseButton.movieId=movieid;
+        GridBagConstraints bC = new GridBagConstraints();
+        bC.gridx = 1 + xshift;
+        bC.gridy = 3 + (index / 3) * 4;
+        bC.ipady = 0;
+        bC.ipadx = 0;
+        bC.gridwidth = 1;
+        bC.gridheight = 1;
+        bC.anchor = GridBagConstraints.NORTH;
+        bC.fill = GridBagConstraints.BOTH;
+        f.add(chooseButton, bC);
+        /*if (xshift == 0 && index != Movies.getNumberOfMovies() - 1) {
             //separator
-            JSeparator separator = new JSeparator(SwingConstants.HORIZONTAL);
+            JPanel separator = new JPanel();
+            separator.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+            //JSeparator separator = new JSeparator(SwingConstants.HORIZONTAL);
             //separator.setPreferredSize(new Dimension(0, 10));
             GridBagConstraints sepC = new GridBagConstraints();
             sepC.gridx = 0;
-            sepC.gridy = 3;
-            sepC.ipady = 10;
-            sepC.gridwidth = 2;
+            sepC.gridy = 3 + (index / 3) * 4;
+            sepC.ipady = 0;
+            sepC.gridwidth = 6;
             //sepC.anchor = GridBagConstraints.CENTER;
             sepC.fill = GridBagConstraints.HORIZONTAL;
             f.add(separator, sepC);
         }*/
+        chooseButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println(chooseButton.movieId);
+            }
+        });
     }
 
     public static void main(String[] args) {
---
+++ src/com/access/user/ShowingChooser.java
@@ -0,0 +1,4 @@
+package com.access.user;
+
+public class ShowingChooser {
+}
