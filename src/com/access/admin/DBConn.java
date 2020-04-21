f7b6604493528320d25f4374cf222dccd5361e55
TrololoManPL <trololomanpl@mailplus.pl>
2020-04-21 21:02:25 +0200

    oczyszczenie DBConn

--- src/com/access/admin/DBConn.java
+++ src/com/access/admin/DBConn.java
@@ -3,44 +3,6 @@ package com.access.admin;
 import java.sql.*;
 
 public abstract class DBConn {
-    public DBConn() throws SQLException {
-    }
-
-    /*public static ResultSet execute(String query) {
-        try {
-//            Class.forName("com.mysql.jdbc.Driver").newInstance();
-            Connection con = DriverManager.getConnection(
-                    "jdbc:mysql://remotemysql.com:3306/w5NtVBqSWa",
-                    "w5NtVBqSWa",
-                    "F6mKAri9L0");
-
-            Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery(query);
-            //con.close();
-
-            return rs;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }*/ //deprecated
-
-    public static void update(String query) { //do usuniecia!
-        try {
-            Class.forName("com.mysql.jdbc.Driver").newInstance();
-            Connection con = DriverManager.getConnection(
-                    "jdbc:mysql://remotemysql.com:3306/w5NtVBqSWa",
-                    "w5NtVBqSWa",
-                    "F6mKAri9L0");
-
-            Statement stmt = con.createStatement();
-            stmt.executeUpdate(query);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
     public static Connection con;
 
     static {
@@ -54,13 +16,8 @@ public abstract class DBConn {
             throwables.printStackTrace();
         }
     }
-    //wtf nie wiem o co chodzi ale dziala i jest duzo szybsze
 
     public static Connection getConnection() {
         return con;
     }
-
-
-
-
 }
